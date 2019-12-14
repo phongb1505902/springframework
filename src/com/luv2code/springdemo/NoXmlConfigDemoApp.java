4d8e675c2d5f103f27e11c1404eb012304fcb08a
Tran Trung Phong <ttphong2019@gmail.com>
2019-12-14 17:24:43 +0700

    Config Annotation No Xml

---
+++ src/com/luv2code/springdemo/NoXmlConfigDemoApp.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class NoXmlConfigDemoApp {
+    public static void main(String[] args){
+        // read spring config file
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SportConfig.class);
+        // get the bean from spring container
+        Coach theCoach = context.getBean("tennisCoach",Coach.class);
+        // call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        // call method get the daily fortune
+        System.out.println(theCoach.getDailyFortune());
+        // close the context
+        context.close();
+    }
+}
